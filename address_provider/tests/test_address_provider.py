10d1d0935394ea8f0248fb3fe4e066907f65f6c1
Bas Dijkstra <bas@ontestautomation.com>
2022-04-15 13:21:53 +0200

    Update provider tests to pull contracts from broker and upload verification results

--- address_provider/tests/test_address_provider.py
+++ address_provider/tests/test_address_provider.py
@@ -1,10 +1,14 @@
 import logging
 import os
 
+import pytest
 from pact import Verifier
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+PACT_BROKER_URL = 'https://ota.pactflow.io'
+PACT_BROKER_TOKEN = 'HbtH0tZq7CU4d18JlKR2kA'
 
 PACT_FILE_CUSTOMER = "customer_consumer_python-address_provider_python.json"
 PACT_FILE_ORDER = "order_consumer_python-address_provider_python.json"
@@ -15,23 +19,37 @@ PACT_URL = f'http://{PACT_MOCK_HOST}:{PACT_MOCK_PORT}'
 PACT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_verify_customer_contract():
+@pytest.fixture
+def broker_options():
+    return {
+        "broker_url": PACT_BROKER_URL,
+        "broker_token": PACT_BROKER_TOKEN,
+        "publish_version": "3",
+        "publish_verification_results": True,
+    }
+
+
+def test_verify_customer_contract(broker_options):
     verifier = Verifier(provider='address_provider_python',
                         provider_base_url=PACT_URL)
 
-    output, logs = verifier.verify_pacts(f'{PACT_DIR}/pacts/{PACT_FILE_CUSTOMER}',
-                                         verbose=False,
-                                         provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL))
+    output, logs = verifier.verify_with_broker(
+        **broker_options,
+        verbose=True,
+        provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL)
+    )
 
     assert (output == 0)
 
 
-def test_verify_order_contract():
+def test_verify_order_contract(broker_options):
     verifier = Verifier(provider='address_provider_python',
                         provider_base_url=PACT_URL)
 
-    output, logs = verifier.verify_pacts(f'{PACT_DIR}/pacts/{PACT_FILE_ORDER}',
-                                         verbose=False,
-                                         provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL))
+    output, logs = verifier.verify_with_broker(
+        **broker_options,
+        verbose=True,
+        provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL)
+    )
 
     assert (output == 0)
