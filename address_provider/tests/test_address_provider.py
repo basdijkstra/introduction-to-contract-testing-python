3cfc3c8f79149f8a0d67e3712eeb23621129fd94
Bas Dijkstra <bas@ontestautomation.com>
2022-04-20 13:26:52 +0200

    Clean up address_provider tests

--- address_provider/tests/test_address_provider.py
+++ address_provider/tests/test_address_provider.py
@@ -9,29 +9,32 @@ logging.basicConfig(level=logging.INFO)
 PACT_FILE_CUSTOMER = "customer_consumer_python-address_provider_python.json"
 PACT_FILE_ORDER = "order_consumer_python-address_provider_python.json"
 
-PACT_MOCK_HOST = '127.0.0.1'
-PACT_MOCK_PORT = 9876
-PACT_URL = f'http://{PACT_MOCK_HOST}:{PACT_MOCK_PORT}'
+APP_HOST = '127.0.0.1'
+APP_PORT = 9876
+APP_URL = f'http://{APP_HOST}:{APP_PORT}'
 PACT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_verify_customer_contract():
     verifier = Verifier(provider='address_provider_python',
-                        provider_base_url=PACT_URL)
+                        provider_base_url=APP_URL)
 
-    output, logs = verifier.verify_pacts(f'{PACT_DIR}/pacts/{PACT_FILE_CUSTOMER}',
-                                         verbose=False,
-                                         provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL))
+    output, logs = verifier.verify_pacts(
+        f'{PACT_DIR}/pacts/{PACT_FILE_CUSTOMER}',
+        verbose=False,
+        provider_states_setup_url=f'{APP_URL}/_pact/provider_states'
+    )
 
     assert (output == 0)
 
 
 def test_verify_order_contract():
     verifier = Verifier(provider='address_provider_python',
-                        provider_base_url=PACT_URL)
+                        provider_base_url=APP_URL)
 
-    output, logs = verifier.verify_pacts(f'{PACT_DIR}/pacts/{PACT_FILE_ORDER}',
-                                         verbose=False,
-                                         provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL))
+    output, logs = verifier.verify_pacts(
+        f'{PACT_DIR}/pacts/{PACT_FILE_ORDER}',
+        verbose=False,
+        provider_states_setup_url=f'{APP_URL}/_pact/provider_states')
 
     assert (output == 0)
