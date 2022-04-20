ac9292aa523540d0a15d824623c89f7459cd8a32
Bas Dijkstra <bas@ontestautomation.com>
2022-04-20 12:57:29 +0200

    Remove duplicate test, rename variables to make more sense

--- address_provider/tests/test_address_provider.py
+++ address_provider/tests/test_address_provider.py
@@ -13,10 +13,9 @@ PACT_BROKER_TOKEN = 'HbtH0tZq7CU4d18JlKR2kA'
 PACT_FILE_CUSTOMER = "customer_consumer_python-address_provider_python.json"
 PACT_FILE_ORDER = "order_consumer_python-address_provider_python.json"
 
-PACT_MOCK_HOST = '127.0.0.1'
-PACT_MOCK_PORT = 9876
-PACT_URL = f'http://{PACT_MOCK_HOST}:{PACT_MOCK_PORT}'
-PACT_DIR = os.path.dirname(os.path.realpath(__file__))
+APP_HOST = '127.0.0.1'
+APP_PORT = 9876
+APP_URL = f'http://{APP_HOST}:{APP_PORT}'
 
 
 @pytest.fixture
@@ -29,27 +28,14 @@ def broker_options():
     }
 
 
-def test_verify_customer_contract(broker_options):
+def test_verify_consumer_contracts(broker_options):
     verifier = Verifier(provider='address_provider_python',
-                        provider_base_url=PACT_URL)
+                        provider_base_url=APP_URL)
 
     output, logs = verifier.verify_with_broker(
         **broker_options,
         verbose=True,
-        provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL)
-    )
-
-    assert (output == 0)
-
-
-def test_verify_order_contract(broker_options):
-    verifier = Verifier(provider='address_provider_python',
-                        provider_base_url=PACT_URL)
-
-    output, logs = verifier.verify_with_broker(
-        **broker_options,
-        verbose=True,
-        provider_states_setup_url="{}/_pact/provider_states".format(PACT_URL)
+        provider_states_setup_url="{}/_pact/provider_states".format(APP_URL)
     )
 
     assert (output == 0)
