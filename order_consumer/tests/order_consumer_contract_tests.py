4057cea20d9faaeb1b786eb2d0057cab321df2d0
Bas Dijkstra <bas@ontestautomation.com>
2025-06-19 15:45:29 +0200

    Add exercises for order_consumer

--- order_consumer/tests/order_consumer_contract_tests.py
+++ order_consumer/tests/order_consumer_contract_tests.py
@@ -11,8 +11,8 @@ logging.basicConfig(level=logging.INFO)
 print(Format().__dict__)
 
 PACT_MOCK_HOST = '127.0.0.1'
-PACT_MOCK_PORT = 1234
-PACT_DIR = os.path.dirname(os.path.realpath(__file__))
+PACT_MOCK_PORT = 9876
+PACT_DIR = f'{os.path.dirname(os.path.realpath(__file__))}/pacts'
 
 EXISTING_ADDRESS = '8aed8fad-d554-4af8-abf5-a65830b49a5f'
 NONEXISTENT_ADDRESS = '00000000-0000-0000-0000-000000000000'
@@ -49,8 +49,9 @@ def test_get_existing_address_id(pact, consumer):
     Add two fields to the expected response:
     - one field 'zipcode', which should be an integer
     - another field 'residential', which should be a boolean
-    Also add an assertion that checks that the mock response
-    returned by the Pact mock server can be read correctly
+
+    Also add assertions that check that the added response fields
+    will be parsed and read correctly if returned by the provider.
     """
 
     expected = {
@@ -62,7 +63,7 @@ def test_get_existing_address_id(pact, consumer):
 
     (pact
      .given(f'Address with ID {EXISTING_ADDRESS} exists')
-     .upon_receiving('a request for address data')
+     .upon_receiving('GET address data - address exists')
      .with_request('get', f'/address/{EXISTING_ADDRESS}')
      .will_respond_with(200, body=Like(expected)))
 
@@ -72,37 +73,22 @@ def test_get_existing_address_id(pact, consumer):
         assert address.number == 123
         assert address.city == 'Nothingville'
 
-
-def test_get_nonexistent_address_id(pact, consumer):
-    (pact
-     .given(f'Address with ID {NONEXISTENT_ADDRESS} does not exist')
-     .upon_receiving('a request for address data')
-     .with_request('get', f'/address/{NONEXISTENT_ADDRESS}')
-     .will_respond_with(404))
-
-    with pact:
-        address = consumer.get_address(NONEXISTENT_ADDRESS)
-        assert address is None
-
+def test_get_nonexistent_address(pact, consumer):
+    """
+    Implement this test by completing the following steps:
+    - Define the pact, including a provider state 'Address with ID {NONEXISTENT_ADDRESS} does not exist'
+    - Specify the request and expect the provider to return an HTTP 404. You don't need to add expectations for the response body
+    - call the get_address() method on the consumer object using NONEXISTENT_ADDRESS as an argument
+    - assert that the response is equal to None
+    """
+    pass
 
 def test_get_invalid_address_id(pact, consumer):
-    (pact
-     .given('No specific state required')
-     .upon_receiving('a request for address data')
-     .with_request('get', f'/address/{INVALID_ADDRESS}')
-     .will_respond_with(400))
-
-    with pact:
-        address = consumer.get_address(INVALID_ADDRESS)
-        assert address is None
-
-
-def test_delete_address_id(pact, consumer):
-    (pact
-     .given('No specific state required')
-     .upon_receiving('a request to delete address data')
-     .with_request('delete', f'/address/{EXISTING_ADDRESS}')
-     .will_respond_with(204))
-
-    with pact:
-        consumer.delete_address(EXISTING_ADDRESS)
+    """
+    Implement this test by completing the following steps:
+    - Define the pact, including a provider state 'No specific state required'
+    - Specify the request and expect the provider to return an HTTP 400. You don't need to add expectations for the response body
+    - call the get_address() method on the consumer object using INVALID_ADDRESS as an argument
+    - assert that the response is equal to None
+    """
+    pass
